6622493614efd395f516da8bf76118f23489b14d
Alysha Neeamuth <alyshanee@gmail.com>
2018-04-27 14:53:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,10 @@
-Function forLoop [0]
+function forLoop (array) {
 
-for(let i = 0 ; i<25; i++) {
+for(let i = 0 ; i<25; i++) 
   if i is == 1 return ("I am 1 strange loop.");
   else if i <25 return ("I am ${i} strange loops.")
+  
+  
+  return array
+
 }
